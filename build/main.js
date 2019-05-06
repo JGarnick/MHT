977268bdcfcc7680f5ba792b508f2c91e2d0fdc3
Josh Garnick <josh@limelightdept.com>
2019-05-06 13:02:11 -0700

    more condition icons added

--- build/main.js
+++ build/main.js
@@ -30,7 +30,7 @@ requireComponent.keys().forEach(fileName => {
                     title: "Monster Health Tracker",
                     rows: [
                         {
-                            name: "The Test", current: "100", max: "100", conditions: ["exhausted", "dying", "deaf"], overlay_class:""
+                            name: "The Test", current: "100", max: "100", conditions: ["blind", "exhausted", "dying", "deaf", "petrified"], overlay_class:""
                         },
                         {
                             name: "The Test 2", current: "100", max: "100", conditions: ["poisoned", "paralyzed", "stunned", "diseased", "cursed"], overlay_class:""
